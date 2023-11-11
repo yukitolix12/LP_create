d211a7c7664c3d5443ccfec160ded7d6f240ef9b
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-11-11 15:24:18 +0900

    click機能をつける

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let boolCloseNav = document.querySelector(".book-close-btn");
+let bookCloseNav = document.querySelector(".book-close-btn");
 let menuBar = document.querySelector(".menu-bar");
+
+bookCloseNav.onclick = () => {
+    menuBar.classList.toggle("active");
+};
